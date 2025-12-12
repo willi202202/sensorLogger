1b0660bbae9cd27223a04ef0e8c803f51e44a2fd
Roman <roman.willi@ost.ch>
2025-12-12 16:33:51 +0100

    e1

--- HTML/update.sh
+++ HTML/update.sh
@@ -3,7 +3,15 @@
 sudo cp index.html /var/www/index.html
 sudo cp pages.json /var/www/pages.json
 
-sudo cp day.html /var/www/day.html
-sudo cp week.html /var/www/week.html
-sudo cp month.html /var/www/month.html
-sudo cp year.html /var/www/year.html
+sudo cp day.html /var/www/weather/day.html
+sudo cp week.html /var/www/weather/week.html
+sudo cp month.html /var/www/weather/month.html
+sudo cp year.html /var/www/weather/year.html
+
+# Eigentümer setzen
+sudo chown -R raspiroman:www-data /var/www/weather
+
+# Rechte setzen
+sudo chmod -R 755 /var/www/weather
+
+sudo systemctl reload nginx
