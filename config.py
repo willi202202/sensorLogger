dd5b6580125ebcba4fea8ad4bea5258b8eb09b0e
Roman <roman.willi@ost.ch>
2025-12-15 22:56:12 +0100

    logger with wind

--- config.py
+++ config.py
@@ -1,59 +1,66 @@
-#!/usr/bin/env python3
 # config.py
 
 # ---------------- MQTT CONFIG ----------------
 BROKER = "127.0.0.1"
 BROKER_PORT = 1883
-TOPIC = "mobilealerts/#"
-CLIENT_ID = "mqtt_sqlite_logger"
+TOPIC = "mobilealerts/+/json"
 
 # ---------------- DATABASE CONFIG ----------------
 DB_FILE = "log/mobilealerts.db"
-TABLE_NAME = "measurements"
-
-# Feldname -> SQL-Typ
-# Namen entsprechen 1:1 den MQTT/JSON Keys
-REQUIRED_FIELDS = {
-    "utms": "TEXT NOT NULL",
-    "temperature1": "REAL",
-    "humidity1": "REAL",
-    "temperature2": "REAL",
-    "humidity2": "REAL",
-    "temperature3": "REAL",
-    "humidity3": "REAL",
-    "temperatureIN": "REAL",
-    "humidityIN": "REAL",
-    "battery": "BOOLEAN"   # wird als 0/1 gespeichert
-}
-
-# Welche Felder sollen als Zahl geparst werden?
-NUMERIC_FIELDS = {
-    "temperature1",
-    "humidity1",
-    "temperature2",
-    "humidity2",
-    "temperature3",
-    "humidity3",
-    "temperatureIN",
-    "humidityIN",
-}
-
-# Welches Feld ist der Battery-Status?
-BATTERY_FIELD = "battery"
-
-# Pflichtfeld für Zeitstempel (NOT NULL in der DB)
-TIMESTAMP_FIELD = "utms"
-
-# ---------------- TIMEOUT / MONITORING ----------------
-TIMEOUT_SECONDS = 3600  # 1 Stunde ohne Daten -> Alarm
 
 # ---------------- MAIL CONFIG ----------------
 MAIL_SENDER = "roman.willi@gmx.ch"
 MAIL_RECIPIENT = "roman.willi@gmx.ch"
 MAIL_SUBJECT_PREFIX = "[MQTT-LOGGER]"
+TIMEOUT_SECONDS = 60 * 60 * 24      # 24h
+DB_SIZE_WARN_MB = 500               # Warnschwelle
+DB_SIZE_CRIT_MB = 900               # kritisch
+DB_SIZE_CHECK_EVERY_MIN = 60 * 60   # z.B. stündlich pruefen
 
-# ---------------- REPORTING CONFIG ----------------
-REPORTS_PATH = "log/reports"
-REPORTS_CONFIG_FILE = "reports.json"
+# ---------------- SENSOR CONFIG ----------------
+TIMESTAMP_FIELD = "utms"
+BATTERY_FIELD = "battery"
 
+# Wie sollen Listen [x,y] behandelt werden
+LIST_POLICY = "first"   # "first" | "last" | "avg"
 
+SENSORS = {
+    "11566802925f": {
+        "table": "measurements_th",
+        "fields": [
+            "utms",
+            "temperature1",
+            "humidity1",
+            "temperature2",
+            "humidity2",
+            "temperature3",
+            "humidity3",
+            "temperatureIN",
+            "humidityIN",
+            "battery",
+        ],
+        "numeric_fields": {
+            "temperature1", "humidity1",
+            "temperature2", "humidity2",
+            "temperature3", "humidity3",
+            "temperatureIN", "humidityIN",
+        },
+    },
+
+    "0b55aada036f": {
+        "table": "measurements_w",
+        "fields": [
+            "utms",
+            "directionDegree",
+            "direction",
+            "windSpeed",
+            "gustSpeed",
+            "battery",
+        ],
+        "numeric_fields": {
+            "directionDegree",
+            "windSpeed",
+            "gustSpeed",
+        },
+    },
+}
